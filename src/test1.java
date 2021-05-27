2f16f4650188b774835c3bf768708f820439a820
ran <kranjith531@gmail.com>
2021-05-27 10:48:17 +0530

    first commit......

---
+++ src/test1.java
@@ -0,0 +1,16 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry ="yes";
+		if(hungry == "yes")
+			System.out.println("eat something ....");
+		else {
+			System.out.println("don't eat something ....");
+
+		}
+	}
+
+}
